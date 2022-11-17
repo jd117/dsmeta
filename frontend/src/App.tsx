5de29d2ba977fd209c9c0cc2cc03796a41c5e8bd
jd117 <juliodiogo@gmail.com>
2022-11-17 18:01:03 +0000

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import NotificationButton from "./components/NotificationButton";
+
 function App() {
 	return (
-		<h1>Hello everyone!
-		</h1>
+		<>
+			<h1>Hello everyone!</h1>
+			<NotificationButton />
+			<NotificationButton />
+		</>
 	);
 }
 
