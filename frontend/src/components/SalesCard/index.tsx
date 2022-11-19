d0941f699f479a944f1f040d2810f50606a55160
jd117 <juliodiogo@gmail.com>
2022-11-19 18:24:53 +0000

    Fix Heroku

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -63,7 +63,7 @@ function SalesCard() {
 						</tr>
 					</thead>
 					<tbody>
-						{sales.map((sale) => {
+						{sales.map(sale => {
 							return (
 								<tr key={sale.id}>
 									<td className="show992">{sale.id}</td>
