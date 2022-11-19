bf2a03511787d7127df04ec5b7a2c1ebb3dcca23
jd117 <juliodiogo@gmail.com>
2022-11-19 10:49:10 +0000

    Send Notification

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
-import NotificationButton from "./components/NotificationButton";
+//import { ToastContainer } from 'react-toastify';
 import Header from "./components/Header";
 import SalesCard from "./components/SalesCard";
 
 function App() {
 	return (
 		<>
+			{/* <ToastContainer /> */}
 			<Header />
 			<main>
 				<section id="sales">
